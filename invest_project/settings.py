60086837a9f71ea552be944f57a1e6593acff1bc
Kozak Yevhen <yevhen.kozak6518@gmail.com>
2026-07-09 14:26:44 +0300

    Update settings.py

--- invest_project/settings.py
+++ invest_project/settings.py
@@ -82,6 +82,8 @@ STATIC_URL = 'static/'
 # ДОДАНО: Папка, куди збиратимуться статичні файли при деплої
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email Testing Config (Виводить листи в консоль)
